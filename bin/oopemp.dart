9982f913c85c6277d3867380f1c06f760aa54d35
muhannad <mohndalzaher@gmail.com>
2024-08-01 14:35:48 +0300

    main modfied and object initiate

--- bin/oopemp.dart
+++ bin/oopemp.dart
@@ -1,3 +1,6 @@
-void main(List<String> arguments) {
-print("hello");
+import 'employtee.dart';
+
+void main(List<String> args) {
+  Employee ahmed =
+      Employee(name: "ahmed", birthday: "1997", salary: 7000, id: 100009980);
 }
