fb99480e78552ffc1e3c59d4bb4402d74f99706c
Mahdi Al-Helal <mmahdi630@gmail.com>
2024-08-01 14:32:36 +0300

    fix nameing

--- bin/employtee.dart
+++ bin/employtee.dart
@@ -3,4 +3,13 @@ class Employee {
   String? birthday;
   double? salary;
   int? id;
+  Employee(
+      {required this.name,
+      required this.birthday,
+      required this.salary,
+      required this.id});
+
+  void empPrint() {
+    print("Name:\n$name ID:\n$id Birthday:\n$birthday Salary:\n$salary");
+  }
 }
