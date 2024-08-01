0e95fc63c79d7700406554195d672daa7da7798b
muhannad <mohndalzaher@gmail.com>
2024-08-01 14:29:31 +0300

    class employee constracter inssite and function

--- bin/employtee.dart
+++ bin/employtee.dart
@@ -3,4 +3,13 @@ class Employee {
   String? birthday;
   double? salary;
   int? id;
+  Employee(
+      {required this.name,
+      required this.birthday,
+      required this.salary,
+      required this.id});
+
+  void empprint() {
+    print("name $name id$id birthday $birthday salary $salary id $id");
+  }
 }
