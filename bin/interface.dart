f608c0f312feaf5fdd515e92601fb2fee7c39dbd
Mahdi Al-Helal <mmahdi630@gmail.com>
2024-08-01 14:52:22 +0300

    add team

--- bin/interface.dart
+++ bin/interface.dart
@@ -4,7 +4,12 @@ void menu() {
   switch (input) {
     case == 1:
       print("1- add 2- edit - delete");
+
     case == 2:
       print("1- add 2- edit - delete");
   }
+}
+
+void empInterface(){
+  
 }
