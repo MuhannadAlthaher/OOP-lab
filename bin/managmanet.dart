27ead220150ad6a2f9fdcd8abeaf2726f00bdcfd
Mahdi Al-Helal <mmahdi630@gmail.com>
2024-08-01 13:56:46 +0300

    create managment class

---
+++ bin/managmanet.dart
@@ -0,0 +1,11 @@
+class Managmanet {
+  late int id;
+  late String name;
+  Managmanet({required this.id, required this.name});
+
+  @override
+  String toString() {
+    // TODO: implement toString
+    return "ID:$id\nName:$name\n";
+  }
+}
